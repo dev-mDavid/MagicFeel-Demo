b7406771f4db5e808043ed2822571b4fd71f4cf3
mDavid <dev.mdavid@gmail.com>
2020-01-06 15:56:31 -0800

    create gitignore file

--- codeBase/animation/above the fold/navLinks/navLinksPrimary/navLinksPrimary001.js
+++ codeBase/animation/above the fold/navLinks/navLinksPrimary/navLinksPrimary001.js
@@ -2,31 +2,27 @@ export function navLinksPrimary() {
   const navLinksPrimaryIterationNum = '001';
   console.log('navLinksPrimary' + navLinksPrimaryIterationNum);
 
-  const tl = new TimelineMax(); // Time Line
-  // Variables
-  const primaryNavLinks = document
-    .getElementsByClassName('Header-nav--primary')[0]
-    .getElementsByTagName('a')
-  // .getElementsByClassName('Header-nav-item')
-
-  // var $navLinks1List = Array.from(document.getElementsByClassName('Header-nav--primary')[0].getElementsByClassName('Header-nav-item'));
-
-  // Set
+  const tl = new TimelineMax();
+  // Bindings
+    const primaryNavLinks = document
+      .getElementsByClassName('Header-nav--primary')[0]
+      .getElementsByTagName('a')
 
   // Animation
-  tl
-    .staggerFrom(
-      primaryNavLinks,
-      // $navLinks1List,
-      // '.Header-nav-item',
-      2, {
-      x: -10,
-      opacity: 0,
-      scale: .9,
-      y: 10,
-      ease: Back.easeOut,
-    },
-      .5
-    );
+    tl
+      .staggerFrom(
+        primaryNavLinks,
+        // $navLinks1List,
+        // '.Header-nav-item',
+        2, {
+        x: -10,
+        opacity: 0,
+        scale: .9,
+        y: 10,
+        ease: Back.easeOut,
+      },
+        .5
+      );
+      
   return (tl)
 }
