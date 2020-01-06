76e19272986eccfbd00479bcfc1f2dbea2adba1a
mDavid <dev.mdavid@gmail.com>
2020-01-06 15:26:17 -0800

    animate navLinksPrimary

--- codeBase/animation/above the fold/aboveTheFold003.js
+++ codeBase/animation/above the fold/aboveTheFold003.js
@@ -1,18 +1,22 @@
 import { navTitle } from "./navTitle/navTitle001";
+import { navLinksPrimary } from "./navLinks/navLinksPrimary/navLinksPrimary001";
 
 export function aboveTheFold() {
   const aboveTheFoldIterationNum = '003';
   console.log('aboveTheFold' + aboveTheFoldIterationNum);
  
   // Master Timeline
-  var master = new TimelineMax();
+    const master = new TimelineMax();
 
-  master
-    .add(navTitle().duration(2))
-    .add('navTitle')
-    
+    master
+      .add(navTitle().duration(2))
+      .add('navTitle')
+      
+      .add(navLinksPrimary().duration(2.5), 'navTitle-=.5')
+      .add('navLinks1')
 
-  console.log("aboveTheFoldTimeLineDuration: " + master.duration())
+
+    console.log("aboveTheFoldTimeLineDuration: " + master.duration())
 
 
 }
--- codeBase/animation/above the fold/navLinks/navLinksPrimary/navLinksPrimary001.js
+++ codeBase/animation/above the fold/navLinks/navLinksPrimary/navLinksPrimary001.js
@@ -1,4 +1,32 @@
 export function navLinksPrimary() {
-  cost navLinksPrimaryIterationNum = '001';
+  const navLinksPrimaryIterationNum = '001';
   console.log('navLinksPrimary' + navLinksPrimaryIterationNum);
+
+  const tl = new TimelineMax(); // Time Line
+  // Variables
+  const primaryNavLinks = document
+    .getElementsByClassName('Header-nav--primary')[0]
+    .getElementsByTagName('a')
+  // .getElementsByClassName('Header-nav-item')
+
+  // var $navLinks1List = Array.from(document.getElementsByClassName('Header-nav--primary')[0].getElementsByClassName('Header-nav-item'));
+
+  // Set
+
+  // Animation
+  tl
+    .staggerFrom(
+      primaryNavLinks,
+      // $navLinks1List,
+      // '.Header-nav-item',
+      2, {
+      x: -10,
+      opacity: 0,
+      scale: .9,
+      y: 10,
+      ease: Back.easeOut,
+    },
+      .5
+    );
+  return (tl)
 }
