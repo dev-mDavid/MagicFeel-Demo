22462a077aa0b4e2a45824554bc910c0630c5641
mDavid <dev.mdavid@gmail.com>
2020-01-06 12:27:06 -0800

    create a new system for finding which modules are being used. run test —successful

--- projects/mDavid/page specific/MagicFeel-Demo.js
+++ projects/mDavid/page specific/MagicFeel-Demo.js
@@ -1,7 +1,18 @@
+
 import { divsFadeIn } from "../../../codeBase/animation/on scroll/divsFadeIn/divsFadeIn001";
 
 export function buildThis() {
 
 divsFadeIn();
+
+
+const modulesUsed = [
+  'divsFadeIn001'
+];
+
+for (let index = 0; index < modulesUsed.length; index++) {
+  console.log(modulesUsed[index]);
+  
+}
 
 };
