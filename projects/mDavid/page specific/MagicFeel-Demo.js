42d8e8f8f75a9bf003f2f2300a8794c5bf29a63d
mDavid <dev.mdavid@gmail.com>
2020-01-06 12:49:49 -0800

    adjust module finder syntax

--- projects/mDavid/page specific/MagicFeel-Demo.js
+++ projects/mDavid/page specific/MagicFeel-Demo.js
@@ -3,16 +3,14 @@ import { divsFadeIn } from "../../../codeBase/animation/on scroll/divsFadeIn/div
 
 export function buildThis() {
 
-divsFadeIn();
+  divsFadeIn();
 
+//Module Finder
+  const modulesUsed = [
+    'divsFadeIn001'
+  ];
 
-const modulesUsed = [
-  'divsFadeIn001'
-];
-
-for (let index = 0; index < modulesUsed.length; index++) {
-  console.log(modulesUsed[index]);
-  
-}
+  console.log("Modules used: ");
+  modulesUsed.forEach(element => console.log("  " + element));
 
 };
