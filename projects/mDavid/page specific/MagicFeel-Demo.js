68cfad719a665e823f12b1ac0b6703b8d5d2f6ec
mDavid <dev.mdavid@gmail.com>
2020-01-07 14:49:23 -0800

    create new onScroll Module

--- projects/mDavid/page specific/MagicFeel-Demo.js
+++ projects/mDavid/page specific/MagicFeel-Demo.js
@@ -1,10 +1,10 @@
-import { afterDOMHasLoaded } from "../../../codeBase/helper services/afterDOMHasLoaded/afterDOMHasLoaded001";
-import { aboveTheFold } from "../../../codeBase/animation/above the fold/aboveTheFold005";
-// import { onScroll } from "../../../codeBase/animation/on scroll/onScroll002";
+// import { afterDOMHasLoaded } from "../../../codeBase/helper services/afterDOMHasLoaded/afterDOMHasLoaded001";
+// import { aboveTheFold } from "../../../codeBase/animation/above the fold/aboveTheFold005";
+import { onScroll } from "../../../codeBase/animation/on scroll/onScroll003";
 export function buildThis() {
-  afterDOMHasLoaded(
-    aboveTheFold()
-    // onScroll();
-  );
+  // afterDOMHasLoaded(
+    // aboveTheFold()
+    onScroll();
+  // );
 
 };
