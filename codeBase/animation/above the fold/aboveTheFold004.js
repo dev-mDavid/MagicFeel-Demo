7cbe49a632420dd1acb81dc35e533701bc2911df
mDavid <dev.mdavid@gmail.com>
2020-01-07 11:30:06 -0800

    sequence master timeline

--- codeBase/animation/above the fold/aboveTheFold004.js
+++ codeBase/animation/above the fold/aboveTheFold004.js
@@ -1,19 +1,20 @@
-export function aboveTheFold() {
-  const aboveTheFoldIterationNum = '004';
-  console.log('aboveTheFold' + aboveTheFoldIterationNum);
+// export function aboveTheFold() {
+  // const aboveTheFoldIterationNum = '004';
+  // console.log('aboveTheFold' + aboveTheFoldIterationNum);
 
   // Start Phase
     // hideBackground
       function startPhase() {
+        var tl = new TimelineMax()
         document.getElementById('magic-feel-opener').style.backgroundColor = 'white';
         // document.getElementsByTagName('h1')[0].style.opacity = 0;
         document.getElementsByTagName('h1')[0].style.color = '#1c1c1c';
         document.getElementsByTagName('h3')[0].style.color = '#1c1c1c';
         document.getElementsByClassName('Header-tagline')[0].style.color = '#4287f5';
+        return (tl)
       }
-      startPhase()
 
-    // Title Reveal
+  // Title Reveal
       function titleReveal() {
         var tl = new TimelineMax()
         // Variables
@@ -54,11 +55,52 @@ export function aboveTheFold() {
         function onComplete() {
           splitTextTitle.revert();
         }
-        ;
+        ; return (tl)
       }
-      titleReveal()
-
+      
   // Subtitle Wave Motion
+      var subtitle = document.getElementsByTagName('h3')[0];
+      var splitTextSubtitle = new SplitText(
+        subtitle, {
+        type: 'chars',
+      });
+
+      function subtitleSnap() {
+        var tl = new TimelineMax()
+        // Variables  
+
+        var subtitleLength = .5;
+        var charsLength = subtitleLength / (subtitleLength * 45);
+        // Set
+
+        // Animation
+        tl
+          .staggerTo(splitTextSubtitle.chars,
+            subtitleLength, {
+            // opacity: 1,
+            // y: 100,
+
+            // y: -10,
+            // x: -5,
+            rotationX: -360,
+            // rotationY: 45,
+            // x: -25,
+            perspective: 400,
+            // y: 100,
+            transformOrigin: '0% -60% -20%',
+            ease: "back.in"
+            // ease: "elastic.inOut(1, 0.3)"
+          },
+            charsLength,
+            '+=0',
+            onComplete
+          );
+
+        function onComplete() {
+          splitTextSubtitle.revert();
+        }
+        ; return (tl)
+      }
 
   // Header Change
   // Video Reveal Background
@@ -66,11 +108,18 @@ export function aboveTheFold() {
 
 
   // Master Timeline
-    const master = new TimelineMax();
+    var master = new TimelineMax();
 
     master
+      .add(startPhase())
+      .add('startPhase')
+
+      .add(titleReveal().duration(3), 'startPhase+=1.5')
+      .add('titleReveal')
+
+      .add(subtitleSnap().duration(2),'titleReveal-=0')
 
     console.log("aboveTheFoldTimeLineDuration: " + master.duration())
 
 
-}
+// }
