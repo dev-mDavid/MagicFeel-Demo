73eda4403dead7a5417f7e5059bd3fbba65d085a
mDavid <dev.mdavid@gmail.com>
2020-01-07 15:34:11 -0800

    develop and style onScroll Module

--- codeBase/animation/on scroll/onScroll003.js
+++ codeBase/animation/on scroll/onScroll003.js
@@ -4,30 +4,68 @@ export function onScroll() {
   const onScrollIterationNum = '003';
   console.log('onScroll' + onScrollIterationNum);
 
-  const
-    h1 = 'h1',
-    h2 = 'h2',
-    h3 = 'h3',
-    p = 'p',
-    hr = 'sqs-block-horizontalrule'
-  ;
 
-  const targetsArray = [
-    h1,
-    h2,
-    h3,
-    p,
-    hr
-  ];
+  
 
-  const fadeInAni = {
-    opacity: 0,
-    y: 25,
-    scale: 0.925,
-    ease: Power1.easeOut,
-    clearProps: 'opacity'
-  };
+  // All targets
+    const
+      h1 = 'h1',
+      h2 = 'h2',
+      h3 = 'h3',
+      p = 'p',
+      hr = 'sqs-block-horizontalrule',
+      icon = 'sci-icon',
+      buttonSmall = 'sqs-block-button-element--small',
+      galleryImg = 'sqs-gallery-design-grid-slide'
 
-  arrayAnimator(targetsArray, fadeInAni);
+    ;
+  
+  // Fade In
+    const fadeInTargets = [
+      h1,
+      h2,
+      h3,
+      p,
+    ];
+    const fadeInAni = {
+      opacity: 0,
+      y: 25,
+      scale: 0.925,
+      ease: Power1.easeOut,
+      clearProps: 'opacity'
+    };
+
+  // Spin In
+    const spinInTargets = [
+      icon
+    ]
+    const spinInAni = {
+      opacity: 0,
+      scale: 0,
+      rotation: "360_ccw",
+      clearProps: 'opacity'
+    }
+
+  // From Center to Out
+    const fromCenterToOutTargets = [
+      buttonSmall,
+      hr,
+
+    ];
+    const fromCenterToOutAni = {
+      scale: 0,
+    }
+  // From Bottom?
+    const fromBottom = [
+      galleryImg
+    ]
+    const fromBottomAni = {
+     x: -200 
+    }
+
+  arrayAnimator(fadeInTargets, fadeInAni);
+  arrayAnimator(spinInTargets, spinInAni);
+  arrayAnimator(fromCenterToOutTargets, fromCenterToOutAni);
+  arrayAnimator(fromBottom, fromBottomAni);
 
 }
