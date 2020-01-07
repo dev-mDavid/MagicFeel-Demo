92efbce6c477480fd83e16eb7c27a0fbd1e191cd
mDavid <dev.mdavid@gmail.com>
2020-01-07 10:13:14 -0800

    copy and paste old similar code to be modified

--- codeBase/animation/above the fold/aboveTheFold004.js
+++ codeBase/animation/above the fold/aboveTheFold004.js
@@ -12,13 +12,51 @@ export function aboveTheFold() {
     startPhase()
 
     // Hidden Title
-  
-    // Title Reveal
-    // Subtitle Wave Motion
+    function navTitle() {
+    var tl = new TimelineMax(); // Time Line
+    // Variables
+    var $navTitle = '.Header-branding';
+    var $STnavTitle = new SplitText(
+      $navTitle, {
+      type: 'chars',
+    });
 
-    // Header Change
-    // Video Reveal Background
-    // Title Color Change
+    var $titleLength = 1.3;
+    var $charsLength = $titleLength / ($titleLength * 50);
+    // Set
+
+    // Animation
+    tl
+      .staggerFrom($STnavTitle.chars,
+        $titleLength, {
+        // opacity: 0,
+        // y: 100,
+
+        y: -10,
+        x: -5,
+        // rotationX: 180,
+        // rotationY: -120,
+        // x: -25,
+        // perspective: 400,
+        // y: 100,
+        // transformOrigin: '0% 60% -60%',
+        ease: Back.easeInOut
+      },
+        $charsLength,
+        '+=0',
+        onComplete
+      );
+
+    function onComplete() {
+      $STnavTitle.revert();
+    }
+    ; return (tl)
+  // Title Reveal
+  // Subtitle Wave Motion
+
+  // Header Change
+  // Video Reveal Background
+  // Title Color Change
 
 
   // Master Timeline
