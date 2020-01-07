dd6618e95c6f614362ef33352068784f9627fd7a
mDavid <dev.mdavid@gmail.com>
2020-01-07 09:53:43 -0800

    comment in the UI motion design

--- codeBase/animation/above the fold/aboveTheFold004.js
+++ codeBase/animation/above the fold/aboveTheFold004.js
@@ -1,7 +1,19 @@
 export function aboveTheFold() {
   const aboveTheFoldIterationNum = '004';
   console.log('aboveTheFold' + aboveTheFoldIterationNum);
- 
+
+  // Start Phase
+    // White Background
+    // Hidden Title
+  
+    // Title Reveal
+    // Subtitle Wave Motion
+
+    // Header Change
+    // Video Reveal Background
+    // Title Color Change
+
+
   // Master Timeline
     const master = new TimelineMax();
 
