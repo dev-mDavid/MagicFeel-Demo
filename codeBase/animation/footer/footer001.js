93afedc4d6c8285fa185120d67226def62057c21
mDavid <dev.mdavid@gmail.com>
2020-01-08 23:16:24 -0800

    animate footer nav folders — failed

--- codeBase/animation/footer/footer001.js
+++ codeBase/animation/footer/footer001.js
@@ -12,6 +12,38 @@ export function footer(){
     footerSetUp();
 
   // event trigger
+    function footerSequence() {
+      let tl = new TimelineMax();
+
+      
+      const firstFooterNavHeader = document.getElementsByClassName('Footer-nav-folder-heading')[0];
+      firstFooterNavHeader.style.opacity = 0;
+      const footerNavHeaders = document.getElementsByClassName('Footer-nav-folder-heading');
+
+      let arrayRemainingFooterNavHeaders = [];
+        for (let index = 1; index < footerNavHeaders.length; index++) {
+          arrayRemainingFooterNavHeaders.push(Array.from(footerNavHeaders[index]))        
+        }
+        console.log(arrayRemainingFooterNavHeaders);
+      let flatArrayRemainingFooterNavHeaders = flatArrayRemainingFooterNavHeaders.flat();
+
+      tl
+        // .to(firstFooterNavHeader, 1, {
+        //   opacity: 1,
+        // })
+        .staggerFrom(flatArrayRemainingFooterNavHeader, 2, {
+          opacity: 0,
+          y: 100,
+        },
+        2,
+        '+=.5'
+        )
+      // reveal first footerNavHeader — word fade in from behind
+      // reveal remaining footerNavHeaders — spread from behind first footerNavHeader
+      // footerNavHeaders wave jump spread revealing the footerNavLinks
+    }
+    footerSequence();
+
     // bottom of the window
     // 75% of the footer (from the bottom)
 
