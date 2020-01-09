07db379f9019d3f0d74bce563728aea4571b8912
mDavid <dev.mdavid@gmail.com>
2020-01-08 22:36:47 -0800

    change footer background color

--- buildThis.js
+++ buildThis.js
@@ -1,6 +1,8 @@
 // import the project animation config file
 // run "parcel build buildThis.js" in the terminal
 
-import { buildThis } from "./projects/mDavid/page specific/MagicFeel-Demo";
-buildThis();
+// import { buildThis } from "./projects/mDavid/page specific/MagicFeel-Demo";
+// buildThis();
 
+import { footer } from "./codeBase/animation/footer/footer001";
+footer();
--- codeBase/animation/footer/footer001.js
+++ codeBase/animation/footer/footer001.js
@@ -2,4 +2,24 @@ export function footer(){
   const footerIterationNum = '001';
   console.log('footer' + footerIterationNum);
 
+  // footer  set up
+    function footerSetUp() {
+      const footer = document.getElementsByTagName('footer')[0];
+      footer.style.backgroundColor = 'none';
+      footer.style.background = 'rgb(0, 0, 0)';
+      footer.style.background = 'linear-gradient(180deg, rgba(0,0,0,1) 32%, rgba(91,57,140,1) 98%)';        
+    }
+    footerSetUp();
+
+  // event trigger
+    // bottom of the window
+    // 75% of the footer (from the bottom)
+
+  // simple reveal animation
+
+  // button movement
+
+  // reset trigger
+    // bottom of the window
+
 }
